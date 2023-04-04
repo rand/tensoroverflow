9973b00e42875d22ca11b08335f91f6525db50ab
Rand Fitzpatrick <rand.fitzpatrick@gmail.com>
2023-04-04 15:47:45 -0400

    build directory

--- src/lib/siteConfig.js
+++ src/lib/siteConfig.js
@@ -3,10 +3,10 @@ export const APPROVED_POSTERS_GH_USERNAME = ['rand'];
 export const GH_USER_REPO = 'rand/tensoroverflow'; // used for pulling github issues and offering comments
 export const SITE_TITLE = 'tensoroverflow';
 export const SITE_DESCRIPTION = "reticulating splines";
-// export const DEFAULT_OG_IMAGE =
-//	'https://user-images.githubusercontent.com/6764957/147861359-3ad9438f-41d1-47c8-aa05-95c7d18497f0.png';
+export const DEFAULT_OG_IMAGE =
+  'https://avatars.githubusercontent.com/u/2835?v=4';
 export const MY_TWITTER_HANDLE = 'sixwing';
-// export const MY_YOUTUBE = 'https://youtube.com/swyxTV';
+export const MY_YOUTUBE = 'https://www.youtube.com/@RandFitzpatrick';
 export const POST_CATEGORIES = ['Essay', 'Note']; // Other categories you can consider adding: Talks, Tutorials, Snippets, Podcasts, Notes...
 export const GH_PUBLISHED_TAGS = ['Published'];
 
